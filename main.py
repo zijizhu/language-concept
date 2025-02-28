c229167d6dbbd7d7d56a8f30eeb7c6916f813e9a
Zhijie Zhu <zhuzhijie1003@outlook.com>
2025-02-28 16:39:15 +1100

    increase default epochs

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def validate(model, test_loader, criterion, device):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs')
     parser.add_argument('--batch-size', type=int, default=64, help='Batch size for training')
     parser.add_argument('--lr', type=float, default=0.01, help='Learning rate')
     parser.add_argument('--data-dir', type=str, default='datasets')
